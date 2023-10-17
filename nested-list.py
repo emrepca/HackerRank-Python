11c2fe90910123d9575a89debb094686f96a755a
emrepca <emrepaca3@gmail.com>
2023-10-17 20:32:34 +0300

    nested-list.py added

--- nested-list.py
+++ nested-list.py
@@ -7,16 +7,19 @@ for _ in range(int(input())):
     alt_list = [name,score]
     students.append(alt_list)
 
-students.sort(key = lambda x:x[1])
-
-x = students[0][1]
-print(f"x: {x}")
-answer = []
-
+students.sort(key = lambda x:x[1]) #sorted by note
+filtered_students = []
+min_score = students[0][1]
 for student in students:
-    if student[1] > x:
-        answer.append(student[0])
-        if len(answer) == 2:
-            break
+    if student[1] > min_score:
+        filtered_students.append(student)
 
-print(answer)
+answer_list = []
+
+for i in range(len(filtered_students)):
+    if filtered_students[0][1] == filtered_students[i][1]:
+        answer_list.append(filtered_students[i])
+        
+answer_list.sort(key = lambda x:x[0]) #sorted by alphabetical order
+for answer in answer_list:
+    print(answer[0])
